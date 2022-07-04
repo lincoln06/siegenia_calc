24abc0e367d32b3919282a167853bb4c4c4d9282
MJL <maciekjl1993@gmail.com>
2022-07-04 15:43:39 +0200

    backup

--- siegenia_calc/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ siegenia_calc/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -277,7 +277,7 @@ namespace siegenia_calc {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/siegenia_calc;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/siegenia_calc;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
